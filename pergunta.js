146ab5ce9a62a5d415ec642393c5d0227da6eb16
eliansomariva <159185075+eliansomariva@users.noreply.github.com>
2024-10-15 16:42:13 -0300

    15/10
    ...

---
+++ pergunta.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'futebol',
+    'Qual jogador ativo hoje em dia tem mais titúlos?',
+    'Lionel Messi.',
+  )
+  
+  criaCartao(
+    'futebol',
+    'Qual segundo jogador com mais titúlos atualmente?',
+    'Cistiano Ronaldo.',
+  )
+  
+  criaCartao(
+    'futebol',   
+    'Quantos titúlos o Messi tem a mas que o Cristiano Ronaldo?',
+    'Nove titúlos.',
+  )
+  
+  criaCartao(
+    'futebol',
+    'Qual jogador com mais lesão atualmente?',
+    'Neymar, com 29 lesões.',
+  
+  )
+  
